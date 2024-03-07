0975b6ae5c4131cac4ba89f9303884e54e8e80b3
mintusikder <mintusikder15@gmail.com>
2024-03-08 00:53:50 +0600

    added error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <h1>Welcome to HeroGadget</h1>
-}
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
-export default App
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <div className="min-h-[calc(100vh-137px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([{
+    path:'/',
+    element:<App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children:[
+        {
+            path:"/",
+           
+            element:<Home></Home>
+        },
+        {
+            path:"/about",
+            element:<About></About>
+        }
+    ]
+}]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router}></RouterProvider>);
